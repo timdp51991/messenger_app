c60e2172c91b3126f69b8448dc52e40708c57488
Timothy Patel <timdp24@gmail.com>
2023-07-26 19:53:49 -0500

    first commit

--- next.config.js
+++ next.config.js
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    experimental: {
+      appDir: true,
+      swcPlugins: [["next-superjson-plugin", {}]]
+    },
+    images: {
+      domains: [
+        'res.cloudinary.com', 
+        'avatars.githubusercontent.com',
+        'lh3.googleusercontent.com'
+      ]
+    }
+  }
+  
+  module.exports = nextConfig
