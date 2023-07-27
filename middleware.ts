a1e85b0c696c2b9e07074a9bc897536e31edc71e
Timothy Patel <timdp24@gmail.com>
2023-07-27 01:27:27 -0500

    fix3

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,8 @@ import { withAuth } from 'next-auth/middleware';
 export default withAuth({
     pages: {
         signIn: "/"
-    }
+    },
+    secret: process.env.NEXTAUTH_SECRET,
 });
 
 export const config = {
